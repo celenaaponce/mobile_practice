a4d72ddc2bd6777c5c3846f5741c8f88d992e07d
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-26 21:18:16 -0700

    Update Pagina_Principal.py

--- Pagina_Principal.py
+++ Pagina_Principal.py
@@ -1,7 +1,16 @@
 import streamlit as st
 from PIL import Image
 
-
+def set_styles():
+    st.write("""
+        <style>
+        h5 {
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+        </style>
+    """, unsafe_allow_html=True)
+  
 image = Image.open('Online world-cuate (2).png')
 
 outer_cols = st.columns([1, 1])
@@ -17,6 +26,7 @@ div.stButton > button:first-child {
 }
 </style>""", unsafe_allow_html=True)
 with outer_cols[0]:
+    set_styles()
     st.markdown("<h1 style='text-align: center; color: white;'>¡Aprender Lengua de Señas Americana en Su Propia Idioma!</h1>", unsafe_allow_html=True)
 
     st.markdown("<h5 style='text-align: center; color: white;'>Para padres Latinos de niños Sordos, sé que hay muchos retos.  Hay bastante que aprender, no solo idioma, pero cultura y más. Mi meta es ayudar padres Latinos entender la lengua de señas americana, sin necesitar saber ingles.  Tambien, quiero ayudar encontrar los recursos que necesitan para que sus hijos pueden tener exitó. </h5>", unsafe_allow_html=True)
