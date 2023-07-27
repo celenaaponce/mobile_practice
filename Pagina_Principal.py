eb68d1e7078070406d1d8bbda56b9221b64e9efd
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-26 20:46:33 -0700

    Update Pagina_Principal.py

--- Pagina_Principal.py
+++ Pagina_Principal.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from PIL import Image
-  
+import streamlit_nested_layout
+
 image = Image.open('image-153x153.jpg')
 
-col1, col2 = st.columns(2)
+outer_cols = st.columns([1, 1])
 m = st.markdown("""
 <style>
 div.stButton > button:first-child {
@@ -15,13 +16,13 @@ div.stButton > button:first-child {
   margin: 0 auto;
 }
 </style>""", unsafe_allow_html=True)
-with col1:
+with outer_cols[0]:
     st.title('¡Aprender Lengua de Señas Americana en Su Propia Idioma!')
     st.caption('Para padres Latinos de niños Sordos, sé que hay muchos retos.  Hay bastante que aprender, no solo idioma, pero cultura y más. Mi meta es ayudar padres Latinos entender la lengua de señas americana, sin necesitar saber ingles.  Tambien, quiero ayudar encontrar los recursos que necesitan para que sus hijos pueden tener exitó.')
 
-with col2:
-    col2a, col2b, col2c = col2
-    with col2b:
+with outer_cols[1]:
+    inner_cols = st.columns([1, 1, 1])
+    with inner_cols[1]:
         st.image(image)
     if st.button('Quien Soy'):
         st.write('Go to quien soy page')
