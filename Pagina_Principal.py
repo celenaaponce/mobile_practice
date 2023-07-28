4df9d0c2ea590e050a42db72ab87623a9668838b
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-28 12:18:53 -0700

    Update Pagina_Principal.py

--- Pagina_Principal.py
+++ Pagina_Principal.py
@@ -12,7 +12,8 @@ def set_styles():
     """, unsafe_allow_html=True)
   
 image = Image.open('Online world-cuate (2).png')
-
+cont_1 = st.container()
+cont_2 = st.container()
 outer_cols = st.columns([1, 1])
 m = st.markdown("""
 <style>
@@ -25,15 +26,30 @@ div.stButton > button:first-child {
   margin: 0 auto;
 }
 </style>""", unsafe_allow_html=True)
-with outer_cols[0]:
-    set_styles()
-    st.markdown("<h1 style='text-align: center; color: white;'>¡Aprender Lengua de Señas Americana en Su Propia Idioma!</h1>", unsafe_allow_html=True)
-
-    st.markdown("<h5 style='text-align: center; color: white;'>Para padres Latinos de niños Sordos, sé que hay muchos retos.  Hay bastante que aprender, no solo idioma, pero cultura y más. Mi meta es ayudar padres Latinos entender la lengua de señas americana, sin necesitar saber ingles.  Tambien, quiero ayudar encontrar los recursos que necesitan para que sus hijos pueden tener exitó. </h5>", unsafe_allow_html=True)
-
-with outer_cols[1]:
-    inner_cols = st.columns([1, 6, 1])
-    with inner_cols[1]:
-        st.image(image)
-    if st.button('Quien Soy'):
-        st.write('Go to quien soy page')
+with cont_1:
+    with outer_cols[0]:
+        set_styles()
+        st.markdown("<h1 style='text-align: center; color: white;'>¡Aprender Lengua de Señas Americana en Su Propia Idioma!</h1>", unsafe_allow_html=True)
+    
+        st.markdown("<h5 style='text-align: center; color: white;'>Para padres Latinos de niños Sordos, sé que hay muchos retos.  Hay bastante que aprender, no solo idioma, pero cultura y más. Mi meta es ayudar padres Latinos entender la lengua de señas americana, sin necesitar saber ingles.  Tambien, quiero ayudar encontrar los recursos que necesitan para que sus hijos pueden tener exitó. </h5>", unsafe_allow_html=True)
+    
+    with outer_cols[1]:
+        inner_cols = st.columns([1, 6, 1])
+        with inner_cols[1]:
+            st.image(image)
+        if st.button('Quien Soy'):
+            st.write('Go to quien soy page')
+outer_col_2 = st.columns([1, 1])            
+with cont_2:
+    with outer_col_2[0]:
+        inner_col_2 = st.columns([1, 6, 1])
+        with inner_col_2[1]:
+            st.image(image)
+        if st.button('Quien Soy'):
+            st.write('Go to quien soy page')
+    
+    with outer_col_2[1]:
+        set_styles()
+        st.markdown("<h1 style='text-align: center; color: white;'>¡Aprender Lengua de Señas Americana en Su Propia Idioma!</h1>", unsafe_allow_html=True)
+    
+        st.markdown("<h5 style='text-align: center; color: white;'>Para padres Latinos de niños Sordos, sé que hay muchos retos.  Hay bastante que aprender, no solo idioma, pero cultura y más. Mi meta es ayudar padres Latinos entender la lengua de señas americana, sin necesitar saber ingles.  Tambien, quiero ayudar encontrar los recursos que necesitan para que sus hijos pueden tener exitó. </h5>", unsafe_allow_html=True)
