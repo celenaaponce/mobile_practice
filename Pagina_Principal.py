3b9a4a37f892d46a7d8f5bdd89c4eac77c34cd27
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-28 18:48:50 -0700

    Update Pagina_Principal.py

--- Pagina_Principal.py
+++ Pagina_Principal.py
@@ -35,9 +35,12 @@ def set_styles():
         </style>
     """, unsafe_allow_html=True)
   
-image = Image.open('Online world-cuate (2).png')
+image = Image.open('/Users/celenap/streamlit/Online world-cuate (2).png')
 cont_1 = st.container()
 cont_2 = st.container()
+cont_3 = st.container()
+cont_4 = st.container()
+cont_5 = st.container()
 outer_cols = st.columns([1, 1])
 
 with cont_1:
@@ -56,7 +59,7 @@ with cont_1:
 
 
 st.divider()
-image2 = Image.open('dictionary.png')
+image2 = Image.open('/Users/celenap/streamlit/dictionary.png')
 
 outer_col_2 = st.columns([1, 1])            
 with cont_2:
@@ -73,3 +76,58 @@ with cont_2:
             st.image(image2)
         st.button('Diccionario', key='Dict')
         ChangeButtonColour('Diccionario', '#fffff', '#407bff') 
+
+st.divider()
+image3 = Image.open('/Users/celenap/streamlit/Online learning-rafiki.png')
+outer_col_3 = st.columns([1, 1])            
+with cont_3:
+    
+    with outer_col_3[0]:
+        set_styles()
+        st.markdown("<h2 style='text-align: center; color: white;'>Clases</h1>", unsafe_allow_html=True)
+    
+        st.markdown("<h5 style='text-align: center; color: white;'>Tomar clases gratis en español para aprender lengua de señas americana. </h5>", unsafe_allow_html=True)
+    
+    with outer_col_3[1]:
+        inner_col_3 = st.columns([1, 6, 1])
+        with inner_col_3[1]:
+            st.image(image3)
+        st.button('Clases', key='Clases')
+        ChangeButtonColour('Clases', '#fffff', '#92E3A9') 
+
+st.divider()
+image4 = Image.open('/Users/celenap/streamlit/Absorbed in-pana.png')
+
+outer_col_4 = st.columns([1, 1])            
+with cont_4:
+    
+    with outer_col_4[1]:
+        set_styles()
+        st.markdown("<h2 style='text-align: center; color: white;'>Libros</h1>", unsafe_allow_html=True)
+    
+        st.markdown("<h5 style='text-align: center; color: white;'>Mirar videos de libros en español con ASL </h5>", unsafe_allow_html=True)
+    
+    with outer_col_4[0]:
+        inner_col_4 = st.columns([1, 6, 1])
+        with inner_col_4[1]:
+            st.image(image4)
+        st.button('Libros', key='Libros')
+        ChangeButtonColour('Libros', '#fffff', '#FF725E') 
+
+st.divider()
+image5 = Image.open('/Users/celenap/streamlit/Selecting team-pana.png')
+outer_col_5 = st.columns([1, 1])            
+with cont_5:
+    
+    with outer_col_5[0]:
+        set_styles()
+        st.markdown("<h2 style='text-align: center; color: white;'>Recursos</h1>", unsafe_allow_html=True)
+    
+        st.markdown("<h5 style='text-align: center; color: white;'>Recursos para familias Latinos con hijos Sordos </h5>", unsafe_allow_html=True)
+    
+    with outer_col_5[1]:
+        inner_col_5 = st.columns([1, 6, 1])
+        with inner_col_5[1]:
+            st.image(image5)
+        st.button('Recursos', key='Recursos')
+        ChangeButtonColour('Recursos', '#fffff', '#C53F3F') 
