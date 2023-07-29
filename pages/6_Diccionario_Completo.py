b200e92e73f3b44e93e2d7a7db9bef58dcaa06c2
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-07-29 12:16:13 -0700

    Update 6_Diccionario_Completo.py

--- pages/6_Diccionario_Completo.py
+++ pages/6_Diccionario_Completo.py
@@ -22,13 +22,13 @@ hide_menu_style = """
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
-with open("streamlit_website/css/style.css") as f:
+with open("css/style.css") as f:
     style = f.read()
 
-with open("streamlit_website/css/bootstrap.css") as file:
+with open("css/bootstrap.css") as file:
     boot = file.read()
 
-with open("streamlit_website/css/responsive.css") as file2:
+with open("css/responsive.css") as file2:
     resp = file2.read()
 
 def remote_css(url):
