a7ea4737e76aaf42030a52998b55b3e2e0b59e4f
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-08-04 17:24:42 -0700

    Update 7_Diccionario_por_Letra.py

--- pages/7_Diccionario_por_Letra.py
+++ pages/7_Diccionario_por_Letra.py
@@ -156,7 +156,7 @@ def print_list(next_list):
           unsafe_allow_html=True)
   
 #start with download
-if st.session_state.download == False:
+if st.session_state.download_letter == False:
   download_csv('1bii0vusXl-640sgVhRK2NVj8XCZtGgDx', 'Search List2.csv')
 word_data = load_words_letra()
 
