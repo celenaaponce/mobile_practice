ef3ca034a9ec62fbbe535b9d214d5fb417c5aed6
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-08-04 20:32:26 -0700

    Update 10_test.py

--- pages/10_test.py
+++ pages/10_test.py
@@ -5,7 +5,7 @@ import streamlit as st
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
 def load_data(sheets_url):
-    csv_url = sheets_url.replace("/view?usp=", "/export?format=csv&gid=")
+    csv_url = sheets_url
     return pd.read_csv(csv_url)
 
 df = load_data(st.secrets["public_gsheets_url"])
