b37bbf571459596f5ee876fc9585fafe34969ee2
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-08-07 18:14:58 -0700

    Update 8_Diccionario_por_Tema.py

--- pages/8_Diccionario_por_Tema.py
+++ pages/8_Diccionario_por_Tema.py
@@ -102,7 +102,7 @@ def get_content(size):
          <p class="caption">Números</p> 
          </div>
         <div class="cat-container">
-         <a href='#' id='Image 19'><img class = "cat" width='{size}%' src='https://images.unsplash.com/photo-1573504286795-36a4a38c52a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGVhc3RlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60'></a>
+         <a href='#' id='Image 19'><img class = "cat" width='{size}%' src='https://images.unsplash.com/photo-1551524612-4b158646bc05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGVhc3RlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60'></a>
          <p class="caption">Pascua</p> 
          </div>
          """
