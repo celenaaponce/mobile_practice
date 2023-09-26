2fdc59818f2120fe20e914c4392ad90299083024
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-09-25 18:56:04 -0700

    Update 9_Buscar_Palabra.py

--- pages/9_Buscar_Palabra.py
+++ pages/9_Buscar_Palabra.py
@@ -103,7 +103,6 @@ if not word_list.empty:
     
 elif not word_list_no_acc.empty:
     link = word_list_no_acc['Video'].to_list()[0]
-    st.write(link)
     word_list = word_data.loc[word_data['Video']==link]
     table = word_list.to_html(classes='mystyle', escape=False, index=False)
 
