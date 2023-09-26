9541073ae6bf97565b140c6fb6206106d88a90c9
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-09-25 18:50:20 -0700

    Update 9_Buscar_Palabra.py

--- pages/9_Buscar_Palabra.py
+++ pages/9_Buscar_Palabra.py
@@ -102,7 +102,7 @@ if not word_list.empty:
         unsafe_allow_html=True)
     
 elif not word_list_no_acc.empty:
-    link = word_list_no_acc[0]['Video']
+    link = word_list_no_acc['Video']
     word_list = word_data.loc[word_data['Video']==link]
     table = word_list.to_html(classes='mystyle', escape=False, index=False)
 
