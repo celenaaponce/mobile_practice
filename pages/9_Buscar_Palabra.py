940b1855436e9965159f28aa2778766782131ddd
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-10-16 19:06:50 -0700

    Update 9_Buscar_Palabra.py

--- pages/9_Buscar_Palabra.py
+++ pages/9_Buscar_Palabra.py
@@ -6,22 +6,7 @@ sys.path.append('streamlit_website/check')
 
 from check.spanish_word_freq import SpanishWordFreq
 from check.word_chekcer import WordChecker
-from st_pages import Page, Section,show_pages, add_page_title
 
-show_pages(
-[
-    Page("Pagina_Principal.py", "Pagina Principal"),
-    Page("pages/1_Diccionario.py", "Diccionario"),
-    Page("pages/2_Clases.py", "Clases"),
-    Page("pages/3_Libros.py", "Libros"),
-    Page("pages/4_Recursos.py", "Recursos"),
-    Page("pages/5_Sobre_Yo.py", "Sobre Yo"),
-    Page("pages/6_Diccionario_Completo.py", "Diccionario Completo"),
-    Page("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra"),
-    Page("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema"),
-    Page("pages/9_Buscar_Palabra.py", "Buscar Palabra"),
-    Page("pages/10_Entrar.py", "Entrar")
-])
 st.set_page_config(layout="wide", page_title="Buscar Palabra")
 
 hide_streamlit_style = """
@@ -62,7 +47,7 @@ def download_csv(file_id, output_file):
 @st.cache_data
 def load_words():
   csv_length = 0    
-  for chunk in pd.read_csv('Search_List2.csv', names=['Palabra', 'Tema', 'Video', 'Imagen', 'Sinómino'], chunksize=10000, skiprows=1):
+  for chunk in pd.read_csv('Search List2.csv', names=['Palabra', 'Tema', 'Video', 'Imagen', 'Sinómino'], chunksize=10000, skiprows=1):
         data = pd.DataFrame(chunk)
   return data
     
@@ -80,7 +65,7 @@ if 'download' not in st.session_state:
     
 #start with download
 if st.session_state.download == False:
-  download_csv(st.secrets['diccionario_letras'], 'Search_List2.csv')
+  download_csv(st.secrets['diccionario_letras'], 'Search List2.csv')
 
 word_data = load_words()
 word_data = word_data[['Palabra', 'Imagen', 'Video', 'Tema', 'Sinómino']]
