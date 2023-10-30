2f45857f82a99e51bd21ebd87c4d5cbd1d330730
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-10-29 21:18:23 -0700

    Update ASLAtHome.py

--- pages/ASLAtHome.py
+++ pages/ASLAtHome.py
@@ -127,3 +127,93 @@ def segunda_semana():
     st.divider()
     
     components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vSb2QAbLy6AheIEJ30YPF-RYjE5gw1Yt7Ovw2wO8Mz2XFUc_MGg2P-8E2i4tlHoiwxXYPGFRE3y1HQn/embed?start=false&loop=false&delayms=3000", height=480)
+
+def tercera_semana():
+    st.subheader('Tercera Semana: Capitulo 1')
+    st.markdown("<h4 style='text-align: center; color: white;'><u>Videos</u></h4>", unsafe_allow_html=True)
+
+    clms1 = st.columns([1,1])
+    with clms1[0]:
+        st.title('')
+        st.markdown('<h5>Vocabulario</h5>', unsafe_allow_html=True)
+    with clms1[1]:
+        st.video('https://youtu.be/F_TOHsNTfwo')
+    clms11 = st.columns([1,1])
+    with clms11[0]:
+        st.title('')
+        st.markdown('<h5>Frases</h5>', unsafe_allow_html=True)
+    with clms11[1]:  
+        st.video('https://youtu.be/9h0xg41ytx4')
+    clms12 = st.columns([1,1])
+    with clms12[0]:
+        st.title('')
+        st.markdown('<h5>Más Frases</h5>', unsafe_allow_html=True)
+    with clms12[1]: 
+        st.video('https://youtu.be/sR9PfilyJ54')
+    clms13 = st.columns([1,1])
+    with clms13[0]:
+        st.title('')
+        st.markdown('<h5>Aun Más Frases</h5>', unsafe_allow_html=True)
+    with clms13[1]: 
+        st.video('https://youtu.be/IwDGq8ZdXeg')
+
+    st.divider()
+
+    st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
+    clms15 = st.columns([1,1])
+    with clms15[0]:
+        st.title('')
+        st.markdown('<h5>Nuevo Vocabulario</h5>', unsafe_allow_html=True)
+    with clms15[1]:
+        st.video('https://youtu.be/7361Wr6W9p4')
+    st.divider()
+    
+    components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vSUy7ujstNW4DmMo_nsX34FvycNSLUA8C8UI8Vrp59GzkF4dJloCFW9qmiYwwV2SaYKVzkW5PFZ7MHy/embed?start=false&loop=false&delayms=3000", height=480)
+
+def segunda_semana():
+    st.subheader('Segunda Semana: Capitulo 1')
+    st.markdown("<h4 style='text-align: center; color: white;'><u>Videos</u></h4>", unsafe_allow_html=True)
+
+    clms1 = st.columns([1,1])
+    with clms1[0]:
+        st.title('')
+        st.markdown('<h5>Vocabulario</h5>', unsafe_allow_html=True)
+    with clms1[1]:
+        st.video('https://youtu.be/F_TOHsNTfwo')
+    clms11 = st.columns([1,1])
+    with clms11[0]:
+        st.title('')
+        st.markdown('<h5>Libro</h5>', unsafe_allow_html=True)
+    with clms11[1]:  
+        st.video('https://youtu.be/q7QTDovFA74')
+    clms12 = st.columns([1,1])
+    with clms12[0]:
+        st.title('')
+        st.markdown('<h5>Vocabulario Extra</h5>', unsafe_allow_html=True)
+    with clms12[1]: 
+        st.video('https://youtu.be/kLiRtHD1Oqc')
+    clms13 = st.columns([1,1])
+    with clms13[0]:
+        st.title('')
+        st.markdown('<h5>Conversación (sin subtítlos) 🔇</h5>', unsafe_allow_html=True)
+    with clms13[1]: 
+        st.video('https://youtu.be/YzButzmbrTw')
+    clms14 = st.columns([1,1])
+    with clms14[0]:
+        st.title('')
+        st.markdown('<h5>Conversación (con subtítlos) 🔈</h5>', unsafe_allow_html=True)
+    with clms14[1]: 
+        st.video('https://youtu.be/BtEIgRvVGg8')
+
+    st.divider()
+
+    st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
+    clms15 = st.columns([1,1])
+    with clms15[0]:
+        st.title('')
+        st.markdown('<h5>Practicar Vocabulario</h5>', unsafe_allow_html=True)
+    with clms15[1]:
+        st.video('https://youtu.be/F_TOHsNTfwo')
+    st.divider()
+    
+    components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vSb2QAbLy6AheIEJ30YPF-RYjE5gw1Yt7Ovw2wO8Mz2XFUc_MGg2P-8E2i4tlHoiwxXYPGFRE3y1HQn/embed?start=false&loop=false&delayms=3000", height=480)
