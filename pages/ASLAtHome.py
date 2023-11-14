f5e7b8e92ddf2bfce657d905c3fef255c0f9d395
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-11-14 08:02:12 -0800

    Update ASLAtHome.py

--- pages/ASLAtHome.py
+++ pages/ASLAtHome.py
@@ -218,3 +218,56 @@ def cuarta_semana():
     st.divider()
     
     components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vS9kBtF4zqp1dtQBzaNHIxG22c8cTTME6K5xL09JldgZ3zX2ONGkYVX52FT0XL22vLjwWM23j0brXyk/embed?start=false&loop=false&delayms=3000", height=480)
+
+def quinta_semana():
+    st.subheader('Quinta Semana: Capitulo 2')
+    st.markdown("<h4 style='text-align: center; color: white;'><u>Videos</u></h4>", unsafe_allow_html=True)
+
+    clms1 = st.columns([1,1])
+    with clms1[0]:
+        st.title('')
+        st.markdown('<h5>Vocabulario</h5>', unsafe_allow_html=True)
+    with clms1[1]:
+        st.video('https://youtu.be/7361Wr6W9p4')
+    clms11 = st.columns([1,1])
+    with clms11[0]:
+        st.title('')
+        st.markdown('<h5>Frases</h5>', unsafe_allow_html=True)
+    with clms11[1]:  
+        st.video('https://youtu.be/LHKtYB_w_RU?si=FwGdPZ9eCoId1TVQ')
+    clms12 = st.columns([1,1])
+    with clms12[0]:
+        st.title('')
+        st.markdown('<h5>Más Frases</h5>', unsafe_allow_html=True)
+    with clms12[1]: 
+        st.video('https://youtu.be/rp3AMCdKxdI?si=mVT9zedbk_dRI2Gj')
+    clms13 = st.columns([1,1])
+    with clms13[0]:
+        st.title('')
+        st.markdown('<h5>Aun Más Frases</h5>', unsafe_allow_html=True)
+    with clms13[1]: 
+        st.video('https://youtu.be/ldSbwWtKf6A?si=QSiW_7Rc-mJXZCHM')
+    clms14 = st.columns([1,1])
+    with clms14[0]:
+        st.title('')
+        st.markdown('<h5>Libro</h5>', unsafe_allow_html=True)
+    with clms14[1]: 
+        st.video('https://youtu.be/BlPrDMZUzkY')    
+    clms15 = st.columns([1,1])
+    with clms15[0]:
+        st.title('')
+        st.markdown('<h5>Otra Frases</h5>', unsafe_allow_html=True)
+    with clms15[1]: 
+        st.video('https://youtu.be/CcxmBTfDlMo')   
+    st.divider()
+
+    st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
+    clms15 = st.columns([1,1])
+    with clms15[0]:
+        st.title('')
+        st.markdown('<h5>Nuevo Vocabulario</h5>', unsafe_allow_html=True)
+    with clms15[1]:
+        st.video('https://youtu.be/9q4e_fQB6No')
+    st.divider()
+    
+    components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQDz2mgnEZK_yCCscpVGqeYxXX1ZYHPER5m1VkECKBv_IPBkdw-RHUf-0kLOI2gBkwx94Cj0reBjJ4W/embed?start=false&loop=false&delayms=3000", height=480)
