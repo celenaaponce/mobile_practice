143e001d4139ed2686048557175f144c393f51cc
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-11-14 16:57:38 -0800

    Update 10_Entrar.py

--- pages/10_Entrar.py
+++ pages/10_Entrar.py
@@ -123,8 +123,8 @@ if authentication_status:
         login_sidebar()
         st.header("Bienvenido a la clase de ASL 2.")
         st.header("Se puede mirar nuestro curiculo aqui:")
-        tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs([":red[Primera Semana]", ":orange[Repaso General]", ":orange[Repaso Leccion 1]", ":orange[Repaso Leccion 2]", 
-                                          ":orange[Repaso Leccion 3]", ":orange[Repaso Leccion 4]", ":orange[Halloween/Dia de los Muertos]", ":green[Tercera Semana]"])
+        tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab 9 = st.tabs([":red[Primera Semana]", ":orange[Repaso General]", ":orange[Repaso Leccion 1]", ":orange[Repaso Leccion 2]", 
+                                          ":orange[Repaso Leccion 3]", ":orange[Repaso Leccion 4]", ":orange[Halloween/Dia de los Muertos]", ":green[Colores]", ":blue[Deletrear]"])
         with tab1:
              ASL2.primera_semana()
         with tab2:
