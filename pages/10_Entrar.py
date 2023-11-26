ce943a6cfe5e54f8f6d1ea7077e46830e1a15283
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-11-26 11:15:38 -0800

    Update 10_Entrar.py

--- pages/10_Entrar.py
+++ pages/10_Entrar.py
@@ -106,8 +106,9 @@ if authentication_status:
         login_sidebar()
         st.header("Bienvenido a la clase de ASL 1.")
         st.header("Se puede mirar nuestro curriculo aqui:")
-        tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs([":white[Primera Semana]", ":white[Conocer la Familia Bravo Pt 1]", ":white[Halloween/Dia de los Muertos]", 
-                                                            ":white[Conocer la Familia Bravo Pt 2]", ":white[Desayuno Pt 1]", ":white[Desayuno Pt 2]", ":white[Dia de Accion de Gracias]"])
+        tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs([":white[Primera Semana]", ":white[Conocer la Familia Bravo Pt 1]", ":white[Halloween/Dia de los Muertos]", 
+                                                            ":white[Conocer la Familia Bravo Pt 2]", ":white[Desayuno Pt 1]", ":white[Desayuno Pt 2]", ":white[Dia de Accion de Gracias]",
+                                                                 ":white[Contról Pt 1]"])
         with tab1:
              ASL1.primera_semana()
         with tab2:
@@ -122,6 +123,8 @@ if authentication_status:
              ASL1.quinta_semana()
         with tab7:
              holidays.thanksgiving()
+        with tab8:
+             ASL1.sexta_semana()
     elif username in st.secrets['ASL2']:
         login_sidebar()
         st.header("Bienvenido a la clase de ASL 2.")
