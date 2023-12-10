c9347c7b1dbe4d466ba37c4c29365e120a1a5093
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-12-09 21:08:49 -0800

    Update ASLAtHome.py

--- pages/ASLAtHome.py
+++ pages/ASLAtHome.py
@@ -29,37 +29,37 @@ def primera_semana():
         st.title('')
         st.markdown('<h5>Alarma de Reloj para personas Sordos</h5>', unsafe_allow_html=True)
     with clms31[1]:
-        st.markdown("<a href='https://www.diglo.com/vibrating-clocks-and-watches/alarm-clocks;d=3;c=31;s=311' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pages/clock.jpg' width='200' height='100'/></a>", unsafe_allow_html=True)     
+        st.markdown("<a href='https://www.diglo.com/vibrating-clocks-and-watches/alarm-clocks;d=3;c=31;s=311' target='_blank'><img style='float: right;' src='https://m.media-amazon.com/images/I/7100+aehKvL._AC_SY300_SX300_.jpg' width='200' height='100'/></a>", unsafe_allow_html=True)     
     clms32 = st.columns([1,1])
     with clms32[0]:
         st.title('')
         st.markdown('<h5>Timbre con luz para personas Sordos</h5>', unsafe_allow_html=True)
     with clms32[1]:    
-        st.markdown("<a href='https://www.diglo.com/shop-by-alert-trigger/doorbell-and-door-knock;d=3;c=32;s=323' target='_blank'><img style='float: right;' src = 'https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pages/light.jpg' width = '200' height = '100'/></a>", unsafe_allow_html=True)     
+        st.markdown("<a href='https://www.diglo.com/shop-by-alert-trigger/doorbell-and-door-knock;d=3;c=32;s=323' target='_blank'><img style='float: right;' src = 'https://m.media-amazon.com/images/I/310R2UqXt0L._AC_.jpg' width = '200' height = '100'/></a>", unsafe_allow_html=True)     
     clms33 = st.columns([1,1])
     with clms33[0]:
         st.title('')
         st.markdown('<h5>Alarma de Incendios para personas Sordos</h5>', unsafe_allow_html=True)
     with clms33[1]:     
-        st.markdown("<a href='https://www.diglo.com/shop-by-alert-trigger/smoke-and-fire;d=3;c=32;s=328' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pages/firealarm.webp' width='200' height = '100'/></a>", unsafe_allow_html=True)
+        st.markdown("<a href='https://www.diglo.com/shop-by-alert-trigger/smoke-and-fire;d=3;c=32;s=328' target='_blank'><img style='float: right;' src='https://m.media-amazon.com/images/I/41JvpGHNyKL.__AC_SX300_SY300_QL70_FMwebp_.jpg' width='200' height = '100'/></a>", unsafe_allow_html=True)
     clms34 = st.columns([1,1])
     with clms34[0]:
         st.title('')
         st.markdown('<h5>Teléfono de Vídeo para personas Sordos</h5>', unsafe_allow_html=True)
     with clms34[1]:         
-        st.markdown("<a href='https://purplevrs.com/espanol' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pages/vp.png' width='150' height = '100'/></a>", unsafe_allow_html=True)
+        st.markdown("<a href='https://purplevrs.com/espanol' target='_blank'><img style='float: right;' src='https://www.purplevrs.com/media/1282/video-quality-slide-v2-450x450.png' width='150' height = '100'/></a>", unsafe_allow_html=True)
     clms35 = st.columns([1,1])
     with clms35[0]:
         st.title('')
         st.markdown('<h5>App para aprender para iPhone </h5>', unsafe_allow_html=True)
     with clms35[1]:      
-        st.markdown("<a href='https://apps.apple.com/us/app/intersign-asl-learn-now/id1567327543' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pages/is.webp' height='100' width='100'/></a>", unsafe_allow_html=True)
+        st.markdown("<a href='https://apps.apple.com/us/app/intersign-asl-learn-now/id1567327543' target='_blank'><img style='float: right;' src='https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/25/02/e4/2502e491-82e5-8ee1-b462-78f521e117f1/AppIcon-1x_U007emarketing-0-7-0-85-220.png/460x0w.webp' height='100' width='100'/></a>", unsafe_allow_html=True)
     clms36 = st.columns([1,1])
     with clms36[0]:
         st.title('')
         st.markdown('<h5>App para aprender para Android</h5>', unsafe_allow_html=True)
     with clms36[1]:     
-        st.markdown("<a href='https://play.google.com/store/apps/details?id=intersign.learn.asl&hl=en_US&gl=US' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pages/is.webp' height='100' width='100'/></a>", unsafe_allow_html=True)
+        st.markdown("<a href='https://play.google.com/store/apps/details?id=intersign.learn.asl&hl=en_US&gl=US' target='_blank'><img style='float: right;' src='https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/25/02/e4/2502e491-82e5-8ee1-b462-78f521e117f1/AppIcon-1x_U007emarketing-0-7-0-85-220.png/460x0w.webp' height='100' width='100'/></a>", unsafe_allow_html=True)
     clms37 = st.columns([1,1])
     with clms37[0]:
         st.title('')
@@ -67,6 +67,7 @@ def primera_semana():
     with clms37[1]: 
         st.video('https://youtu.be/Pt2_EjmtUp8')
 
+    st.divider()
     
     st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
     clms38 = st.columns([1,1])
@@ -75,7 +76,7 @@ def primera_semana():
         st.markdown('<h5>Vocabulario para la semana que viene</h5>', unsafe_allow_html=True)
     with clms38[1]:
         st.video('https://youtu.be/F_TOHsNTfwo')
-
+    st.divider()
     
     components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQ4wlJOjhmNap4RDFiDtqNi1cv2PvEsdZnP4ANcRsVCCDgK0NrpYYLfI5BgwVZzlycwNwmvlwU4qnNt/embed?start=false&loop=false&delayms=3000", height=480)
 
@@ -114,7 +115,7 @@ def segunda_semana():
     with clms14[1]: 
         st.video('https://youtu.be/BtEIgRvVGg8')
 
-
+    st.divider()
 
     st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
     clms15 = st.columns([1,1])
@@ -123,6 +124,7 @@ def segunda_semana():
         st.markdown('<h5>Practicar Vocabulario</h5>', unsafe_allow_html=True)
     with clms15[1]:
         st.video('https://youtu.be/F_TOHsNTfwo')
+    st.divider()
     
     components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vSb2QAbLy6AheIEJ30YPF-RYjE5gw1Yt7Ovw2wO8Mz2XFUc_MGg2P-8E2i4tlHoiwxXYPGFRE3y1HQn/embed?start=false&loop=false&delayms=3000", height=480)
 
@@ -155,6 +157,7 @@ def tercera_semana():
     with clms13[1]: 
         st.video('https://youtu.be/IwDGq8ZdXeg')
 
+    st.divider()
 
     st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
     clms15 = st.columns([1,1])
@@ -163,7 +166,7 @@ def tercera_semana():
         st.markdown('<h5>Nuevo Vocabulario</h5>', unsafe_allow_html=True)
     with clms15[1]:
         st.video('https://youtu.be/7361Wr6W9p4')
-
+    st.divider()
     
     components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vSUy7ujstNW4DmMo_nsX34FvycNSLUA8C8UI8Vrp59GzkF4dJloCFW9qmiYwwV2SaYKVzkW5PFZ7MHy/embed?start=false&loop=false&delayms=3000", height=480)
 
@@ -189,7 +192,7 @@ def cuarta_semana():
         st.markdown('<h5>Vocabulario Extra</h5>', unsafe_allow_html=True)
     with clms12[1]: 
         st.video('https://youtu.be/gxGKiFP7G0U')
-
+    st.divider()
     clms13 = st.columns([1,1])
     with clms13[0]:
         st.title('')
@@ -203,6 +206,7 @@ def cuarta_semana():
     with clms14[1]: 
         st.video('https://youtu.be/tvyr6wu4dL4')
 
+    st.divider()
 
     st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
     clms15 = st.columns([1,1])
@@ -211,7 +215,7 @@ def cuarta_semana():
         st.markdown('<h5>Practicar Vocabulario</h5>', unsafe_allow_html=True)
     with clms15[1]:
         st.video('https://youtu.be/7361Wr6W9p4')
-
+    st.divider()
     
     components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vS9kBtF4zqp1dtQBzaNHIxG22c8cTTME6K5xL09JldgZ3zX2ONGkYVX52FT0XL22vLjwWM23j0brXyk/embed?start=false&loop=false&delayms=3000", height=480)
 
@@ -255,7 +259,7 @@ def quinta_semana():
         st.markdown('<h5>Otra Frases</h5>', unsafe_allow_html=True)
     with clms15[1]: 
         st.video('https://youtu.be/CcxmBTfDlMo')   
-
+    st.divider()
 
     st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
     clms15 = st.columns([1,1])
@@ -264,6 +268,6 @@ def quinta_semana():
         st.markdown('<h5>Nuevo Vocabulario</h5>', unsafe_allow_html=True)
     with clms15[1]:
         st.video('https://youtu.be/9q4e_fQB6No')
-
+    st.divider()
     
     components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQDz2mgnEZK_yCCscpVGqeYxXX1ZYHPER5m1VkECKBv_IPBkdw-RHUf-0kLOI2gBkwx94Cj0reBjJ4W/embed?start=false&loop=false&delayms=3000", height=480)
