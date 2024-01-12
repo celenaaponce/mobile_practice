c7f19d62d0fbacc2d1cbd982be43f2312c1bf567
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-01-11 18:24:33 -0800

    Update 7_Diccionario_por_Letra.py

--- pages/7_Diccionario_por_Letra.py
+++ pages/7_Diccionario_por_Letra.py
@@ -156,8 +156,7 @@ def print_list(next_list):
   
 #start with download
 if st.session_state.download_letter == False:
-  download_csv(st.secrets['diccionario_letras'], 'Search List2.csv')
-word_data = load_words_letra()
+  word_data = download_csv(st.secrets['diccionario_letras'], 'Search List2.csv')
 word_data = word_data[['Palabra', 'Imagen', 'Video', 'Tema', 'Sinómino']]
 
 #set up main page with images  
