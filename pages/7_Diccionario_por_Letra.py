7fea5392aecc34a1bdec24c009d4dda02e092bac
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-01-25 16:41:58 -0800

    Update 7_Diccionario_por_Letra.py

--- pages/7_Diccionario_por_Letra.py
+++ pages/7_Diccionario_por_Letra.py
@@ -21,7 +21,8 @@ show_pages(
     Page("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra"),
     Page("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema"),
     Page("pages/9_Buscar_Palabra.py", "Buscar Palabra"),
-    Page("pages/10_Entrar.py", "Entrar")
+    Page("pages/10_Entrar.py", "Entrar"),
+    Page("pages/11_Form.py", "Registrar para Clases")
 ])
 ##constants
 alpha_num = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k',
