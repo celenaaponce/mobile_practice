dc75c344a1a62ebfe5c456b3f681ec44a2594977
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-02-24 23:15:48 -0800

    Update Bravo_6.py

--- pages/Bravo_6.py
+++ pages/Bravo_6.py
@@ -43,10 +43,10 @@ def login_sidebar_ASL2():
         Page("pages/9_Buscar_Palabra.py", "Buscar Palabra"),
         Page("pages/10_Entrar.py", "Entrar"),
         Page("pages/Introduccion_a_ASL_2.py", "Introducción a ASL 2"),
-        Page("pages/Bravo_5.py", "Repaso"),
-        Page("pages/Bravo_6.py", "Colores y Deletrear"),
-        Page("pages/Bravo_7.py", "Escuela"),
-        Page("pages/holidays.py", "Días Festivos")
+        # Page("pages/Bravo_5.py", "Repaso"),
+        # Page("pages/Bravo_6.py", "Colores y Deletrear"),
+        # Page("pages/Bravo_7.py", "Escuela"),
+        Page("pages/holidays_spring_2.py", "Días Festivos")
     ]
 )
         
