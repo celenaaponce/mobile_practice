6018c2d92a1a005e50ae5445b0b2597187ff367f
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-02-24 23:39:03 -0800

    Update Introduccion_a_ASL_1.py

--- pages/Introduccion_a_ASL_1.py
+++ pages/Introduccion_a_ASL_1.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 from streamlit_extras.switch_page_button import switch_page
 import streamlit.components.v1 as components
-from pages import holidays
+# from pages import holidays
 from st_pages import Page, Section,show_pages, add_page_title
 
 
