6bc122e9c1a77f1262968e610cda7618af78adcf
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-22 12:49:34 -0700

    Update nav.py

--- modules/nav.py
+++ modules/nav.py
@@ -47,7 +47,22 @@ def ASL2_sidebar():
             st.sidebar.page_link("pages/Introduccion_a_ASL_2.py", label="Introducción a ASL 2")
             st.sidebar.page_link("pages/Bravo_4.py", label="Ir de Compras")
             st.sidebar.page_link("pages/holidays_spring_2.py", label="Días Festivos")
-
+            
+def ASL3_sidebar():
+            st.sidebar.page_link("Pagina_Principal.py", label="Pagina Principal")
+            st.sidebar.page_link("pages/Diccionario.py", label="Diccionario")
+            st.sidebar.page_link("pages/Clases.py", label="Clases")
+            st.sidebar.page_link("pages/Libros.py", label="Libros")
+            st.sidebar.page_link("pages/Recursos.py", label="Recursos")
+            st.sidebar.page_link("pages/Sobre_Yo.py", label="Sobre Yo")
+            st.sidebar.page_link("pages/Diccionario_Completo.py", label="Diccionario Completo")
+            st.sidebar.page_link("pages/Diccionario_por_Letra.py", label="Diccionario Por Letra")
+            st.sidebar.page_link("pages/Diccionario_por_Tema.py", label="Diccionario Por Tema")
+            st.sidebar.page_link("pages/Buscar_Palabra.py", label="Buscar Palabra")
+            st.sidebar.page_link("pages/Entrar.py", label="Entrar")
+            st.sidebar.page_link("pages/Introduccion_a_ASL_3.py", label="Introducción a ASL 3")
+            st.sidebar.page_link("pages/Bravo_7.py", label="Escuela")
+            st.sidebar.page_link("pages/holidays_spring_3.py", label="Días Festivos")
 
 def MenuButtons(user_roles = ''):
     if user_roles == '':
@@ -58,6 +73,9 @@ def MenuButtons(user_roles = ''):
 
     elif user_roles == 'ASL2':
             ASL2_sidebar()
+
+    elif user_roles == 'ASL3':
+            ASL3_sidebar()
                 
     if 'authentication_status' not in ss:
         ss.authentication_status = False
