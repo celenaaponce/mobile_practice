104da111254817c129d9f30f8de719ee802103ba
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-22 12:52:16 -0700

    Update Introduccion_a_ASL_3.py

--- pages/helper/Introduccion_a_ASL_3.py
+++ pages/helper/Introduccion_a_ASL_3.py
@@ -2,10 +2,10 @@ import streamlit as st
 from PIL import Image
 from streamlit_extras.switch_page_button import switch_page
 import streamlit.components.v1 as components
-from pages.sidebars import login_sidebar_ASL3, set_styles
-st.write(st.session_state)
+from modules.nav import MenuButtons
+
 def main():
-    login_sidebar_ASL3()
+    MenuButtons('ASL3')
     st.header("Bienvenido a la clase de ASL 3.")
     st.header("Se puede mirar nuestro curriculo aqui:")
     primera_semana()
@@ -21,43 +21,43 @@ def primera_semana():
         st.title('')
         st.markdown('<h5>Escuelas para los Sordos en EEUU</h5>', unsafe_allow_html=True)
     with clms[1]:
-        st.markdown("<a href='https://www.asd-1817.org/deaf-schools' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pages/deaf-hard-hearing-education-tc-columbia.jpg' width= '200' height = '100' /></a>", unsafe_allow_html=True)
+        st.markdown("<a href='https://www.asd-1817.org/deaf-schools' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/deaf-hard-hearing-education-tc-columbia.jpg' width= '200' height = '100' /></a>", unsafe_allow_html=True)
     clms31 = st.columns([1,1])
     with clms31[0]:
         st.title('')
         st.markdown('<h5>Alarma de Reloj para personas Sordos</h5>', unsafe_allow_html=True)
     with clms31[1]:
-        st.markdown("<a href='https://www.diglo.com/vibrating-clocks-and-watches/alarm-clocks;d=3;c=31;s=311' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pages/clock.jpg' width='200' height='100'/></a>", unsafe_allow_html=True)     
+        st.markdown("<a href='https://www.diglo.com/vibrating-clocks-and-watches/alarm-clocks;d=3;c=31;s=311' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/clock.jpg' width='200' height='100'/></a>", unsafe_allow_html=True)     
     clms32 = st.columns([1,1])
     with clms32[0]:
         st.title('')
         st.markdown('<h5>Timbre con luz para personas Sordos</h5>', unsafe_allow_html=True)
     with clms32[1]:    
-        st.markdown("<a href='https://www.diglo.com/shop-by-alert-trigger/doorbell-and-door-knock;d=3;c=32;s=323' target='_blank'><img style='float: right;' src = 'https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pages/light.jpg' width = '200' height = '100'/></a>", unsafe_allow_html=True)     
+        st.markdown("<a href='https://www.diglo.com/shop-by-alert-trigger/doorbell-and-door-knock;d=3;c=32;s=323' target='_blank'><img style='float: right;' src = 'https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/light.jpg' width = '200' height = '100'/></a>", unsafe_allow_html=True)     
     clms33 = st.columns([1,1])
     with clms33[0]:
         st.title('')
         st.markdown('<h5>Alarma de Incendios para personas Sordos</h5>', unsafe_allow_html=True)
     with clms33[1]:     
-        st.markdown("<a href='https://www.diglo.com/shop-by-alert-trigger/smoke-and-fire;d=3;c=32;s=328' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pages/firealarm.webp' width='200' height = '100'/></a>", unsafe_allow_html=True)
+        st.markdown("<a href='https://www.diglo.com/shop-by-alert-trigger/smoke-and-fire;d=3;c=32;s=328' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/firealarm.webp' width='200' height = '100'/></a>", unsafe_allow_html=True)
     clms34 = st.columns([1,1])
     with clms34[0]:
         st.title('')
         st.markdown('<h5>Teléfono de Vídeo para personas Sordos</h5>', unsafe_allow_html=True)
     with clms34[1]:         
-        st.markdown("<a href='https://purplevrs.com/espanol' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pages/vp.png' width='150' height = '100'/></a>", unsafe_allow_html=True)
+        st.markdown("<a href='https://purplevrs.com/espanol' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/vp.png' width='150' height = '100'/></a>", unsafe_allow_html=True)
     clms35 = st.columns([1,1])
     with clms35[0]:
         st.title('')
         st.markdown('<h5>App para aprender para iPhone </h5>', unsafe_allow_html=True)
     with clms35[1]:      
-        st.markdown("<a href='https://apps.apple.com/us/app/intersign-asl-learn-now/id1567327543' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pages/is.webp' height='100' width='100'/></a>", unsafe_allow_html=True)
+        st.markdown("<a href='https://apps.apple.com/us/app/intersign-asl-learn-now/id1567327543' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/is.webp' height='100' width='100'/></a>", unsafe_allow_html=True)
     clms36 = st.columns([1,1])
     with clms36[0]:
         st.title('')
         st.markdown('<h5>App para aprender para Android</h5>', unsafe_allow_html=True)
     with clms36[1]:     
-        st.markdown("<a href='https://play.google.com/store/apps/details?id=intersign.learn.asl&hl=en_US&gl=US' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pages/is.webp' height='100' width='100'/></a>", unsafe_allow_html=True)
+        st.markdown("<a href='https://play.google.com/store/apps/details?id=intersign.learn.asl&hl=en_US&gl=US' target='_blank'><img style='float: right;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/is.webp' height='100' width='100'/></a>", unsafe_allow_html=True)
     clms37 = st.columns([1,1])
     with clms37[0]:
         st.title('')
