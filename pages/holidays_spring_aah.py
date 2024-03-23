3d51a2ef487a5eb38883dbd8ec88da9c683fc9f9
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-22 20:12:19 -0700

    Update holidays_spring_aah.py

--- pages/holidays_spring_aah.py
+++ pages/holidays_spring_aah.py
@@ -14,10 +14,14 @@ def main():
     MenuButtons('ASLEnCasa')
     st.header("Bienvenido a la clase de ASL En Casa.")
     st.header("Se puede mirar nuestro curriculo aqui:")
-    tab1, = st.tabs([ ":white[Primavera]"])
+    tab1, = st.tabs([ ":white[Primavera]", ":white[Semana Santa]", ":white[Paises Latinos]"])
 
     with tab1:
             spring()
+    with tab2:
+            semana_santa()
+    with tab3:
+            pais_latino()
 
 
 def spring():
@@ -46,7 +50,75 @@ def spring():
     with clms3[1]:
         st.video('https://youtu.be/spE7JQHC9Zw')
     components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQWQGDfD9frtoRelg1LUY82AfX61Uncc62N2cOlLlR6m_rVaBFQLd0y7sEROoLbOwN3LHV0aCIIcHPA/embed?start=false&loop=false&delayms=3000", height=480)
-
+def semana_santa():
+    clms = st.columns([1,1])
+    with clms[0]:
+        st.title('')
+        st.markdown('<h5>Vocabulario General</h5>', unsafe_allow_html=True)
+    with clms[1]:
+        st.video('https://youtu.be/ldm6mEvjHsk')
+    clms1 = st.columns([1,1])
+    with clms1[0]:
+        st.title('')
+        st.markdown('<h5>Domingo de Ramos</h5>', unsafe_allow_html=True)
+    with clms1[1]:
+        st.video('https://youtu.be/NiVsgq8aE7Y')
+    clms2 = st.columns([1,1])
+    with clms2[0]:
+        st.title('')
+        st.markdown('<h5>Jueves Santo</h5>', unsafe_allow_html=True)
+    with clms2[1]:
+        st.video('https://youtu.be/4q3hgVOLsnc')
+    clms3 = st.columns([1,1])
+    with clms3[0]:
+        st.title('')
+        st.markdown('<h5>Viernes Santo</h5>', unsafe_allow_html=True)
+    with clms3[1]:
+        st.video('https://youtu.be/4q3hgVOLsnc')  
+    clms4 = st.columns([1,1])
+    with clms4[0]:
+        st.title('')
+        st.markdown('<h5>Cuento de Pascua</h5>', unsafe_allow_html=True)
+    with clms4[1]:
+        st.video('https://youtu.be/RONMK52bc8w') 
+        
+def pais_latino():
+    clms = st.columns([1,1])
+    with clms[0]:
+        st.title('')
+        st.markdown('<h5>America Central</h5>', unsafe_allow_html=True)
+    with clms[1]:
+        st.video('https://youtu.be/8MXqNlUBLPE')
+    clms1 = st.columns([1,1])
+    with clms1[0]:
+        st.title('')
+        st.markdown('<h5>America del Sur</h5>', unsafe_allow_html=True)
+    with clms1[1]:
+        st.video('https://youtu.be/2MH7znNtk-o')
+    clms2 = st.columns([1,1])
+    with clms2[0]:
+        st.title('')
+        st.markdown('<h5>Puerto Rico</h5>', unsafe_allow_html=True)
+    with clms2[1]:
+        st.video('https://www.youtube.com/watch?v=NNZD9RAgL4w')
+    clms3 = st.columns([1,1])
+    with clms3[0]:
+        st.title('')
+        st.markdown('<h5>Cuba</h5>', unsafe_allow_html=True)
+    with clms3[1]:
+        st.video('https://www.youtube.com/watch?v=lDmOnxjPNds')  
+    clms4 = st.columns([1,1])
+    with clms4[0]:
+        st.title('')
+        st.markdown('<h5>Republica Dominicana</h5>', unsafe_allow_html=True)
+    with clms4[1]:
+        st.video('https://www.youtube.com/watch?v=mKTAsHCqDeg') 
+    clms5 = st.columns([1,1])
+    with clms5[0]:
+        st.title('')
+        st.markdown('<h5>America del Norte</h5>', unsafe_allow_html=True)
+    with clms5[1]:
+        st.video('https://youtu.be/v-3nyi1nlO8') 
 
 
 main()
