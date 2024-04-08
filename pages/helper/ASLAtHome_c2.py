8a9c4ef9b8b33db00d2ee55e9dcfd4468abc958a
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-04-08 11:09:49 -0700

    Update ASLAtHome_c2.py

--- pages/helper/ASLAtHome_c2.py
+++ pages/helper/ASLAtHome_c2.py
@@ -3,54 +3,27 @@ from PIL import Image
 from streamlit_extras.switch_page_button import switch_page
 import streamlit.components.v1 as components
 from st_pages import Page, Section,show_pages, add_page_title
-
-
-
+from modules.nav import MenuButtons
+from pages.sidebars import set_styles
+st.markdown("""
+    <style>
+        div[data-testid="stSidebarNav"]{
+            display: none !important;
+        }
+    </style>
+    """, unsafe_allow_html=True)
 def main():
-    login_sidebar_ASLAtHome2()
+    MenuButtons("ASLEnCasa")
+    set_styles("#94387f")
     st.header("Bienvenido a la clase de ASL En Casa.")
     st.header("Se puede mirar nuestro curriculo aqui:")
-    tab2, tab4 = st.tabs([":white[Capitulo 2 Pt 1]",  ":white[Capitulo 2 Pt 2]"])
+    tab2 = st.tabs([":white[Capítulo 2]"])
 
     with tab2:
-            cuarta_semana()
-    with tab4:
-            quinta_semana()
+            segunda_semana()
 
-def login_sidebar_ASLAtHome2():
-        show_pages(
-    [
-        Page("Pagina_Principal.py", "Pagina Principal"),
-        Page("pages/1_Diccionario.py", "Diccionario"),
-        Page("pages/2_Clases.py", "Clases"),
-        Page("pages/3_Libros.py", "Libros"),
-        Page("pages/4_Recursos.py", "Recursos"),
-        Page("pages/5_Sobre_Yo.py", "Sobre Yo"),
-        Page("pages/6_Diccionario_Completo.py", "Diccionario Completo"),
-        Page("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra"),
-        Page("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema"),
-        Page("pages/9_Buscar_Palabra.py", "Buscar Palabra"),
-        Page("pages/10_Entrar.py", "Entrar"),
-        Page("pages/Introduccion_a_ASL_En_Casa.py"),
-        Page("pages/ASLAtHome_c1.py", "Capitulo 1"),
-        Page("pages/ASLAtHome_c2.py", "Capitulo 2"),
-        Page("pages/holidays_aah.py", "Dias Festivos")
-    ]
-)
-
-def set_styles():
-    st.write("""
-        <style>
-        a {
-            background-color: #94387f;
-            color: white;
-
-            }
-        </style>
-    """, unsafe_allow_html=True)
-
-def cuarta_semana():
-    st.subheader('Capitulo 2 Pt 1')
+def segunda_semana():
+    st.subheader('Capítulo 2')
     st.markdown("<h4 style='text-align: center; color: white;'><u>Videos</u></h4>", unsafe_allow_html=True)
 
     clms1 = st.columns([1,1])
@@ -58,97 +31,72 @@ def cuarta_semana():
         st.title('')
         st.markdown('<h5>Vocabulario</h5>', unsafe_allow_html=True)
     with clms1[1]:
-        st.video('https://youtu.be/7361Wr6W9p4')
+        st.video('https://www.youtube.com/watch?v=RNjed7e9vXM')
+    clms21 = st.columns([1,1])
+    with clms21[0]:
+        st.title('')
+        st.markdown('<h5>Clasificadores</h5>', unsafe_allow_html=True)
+    with clms21[1]:
+        st.video('https://youtube.com/watch?v=I-i1Hsp_h-s&t=18sM')    
     clms11 = st.columns([1,1])
     with clms11[0]:
         st.title('')
-        st.markdown('<h5>Libro de Hora de Bañarse</h5>', unsafe_allow_html=True)
+        st.markdown('<h5>Frases de Practica</h5>', unsafe_allow_html=True)
     with clms11[1]:  
-        st.video('https://youtu.be/4TmdTAaPYdA')
+        st.video('https://www.youtube.com/watch?v=TLBdOJycKug')
     clms12 = st.columns([1,1])
     with clms12[0]:
         st.title('')
-        st.markdown('<h5>Vocabulario Extra</h5>', unsafe_allow_html=True)
+        st.markdown('<h5>Frases Receptivas</h5>', unsafe_allow_html=True)
     with clms12[1]: 
-        st.video('https://youtu.be/gxGKiFP7G0U')
-    st.divider()
+        st.video('https://www.youtube.com/watch?v=TLBdOJycKug')
     clms13 = st.columns([1,1])
     with clms13[0]:
         st.title('')
-        st.markdown('<h5>Vocabulario para de la Cabeza a los Pies</h5>', unsafe_allow_html=True)
+        st.markdown('<h5>Escenarios</h5>', unsafe_allow_html=True)
     with clms13[1]: 
-        st.video('https://youtu.be/UV-SadXiTh0')
-    clms14 = st.columns([1,1])
-    with clms14[0]:
-        st.title('')
-        st.markdown('<h5>Libro de la Cabeza a los Pies</h5>', unsafe_allow_html=True)
-    with clms14[1]: 
-        st.video('https://youtu.be/tvyr6wu4dL4')
-
+        st.video('https://www.youtube.com/watch?v=r8fk2V9PeXQ&t=3s')
     st.divider()
-
-    st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
-    clms15 = st.columns([1,1])
-    with clms15[0]:
-        st.title('')
-        st.markdown('<h5>Practicar Vocabulario</h5>', unsafe_allow_html=True)
-    with clms15[1]:
-        st.video('https://youtu.be/7361Wr6W9p4')
-    st.divider()
-    
-    components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vS9kBtF4zqp1dtQBzaNHIxG22c8cTTME6K5xL09JldgZ3zX2ONGkYVX52FT0XL22vLjwWM23j0brXyk/embed?start=false&loop=false&delayms=3000", height=480)
-
-def quinta_semana():
-    st.subheader('Capitulo 2 Pt 2')
-    st.markdown("<h4 style='text-align: center; color: white;'><u>Videos</u></h4>", unsafe_allow_html=True)
-
-    clms1 = st.columns([1,1])
-    with clms1[0]:
-        st.title('')
-        st.markdown('<h5>Vocabulario</h5>', unsafe_allow_html=True)
-    with clms1[1]:
-        st.video('https://youtu.be/7361Wr6W9p4')
-    clms11 = st.columns([1,1])
-    with clms11[0]:
-        st.title('')
-        st.markdown('<h5>Frases</h5>', unsafe_allow_html=True)
-    with clms11[1]:  
-        st.video('https://youtu.be/LHKtYB_w_RU?si=FwGdPZ9eCoId1TVQ')
-    clms12 = st.columns([1,1])
-    with clms12[0]:
-        st.title('')
-        st.markdown('<h5>Más Frases</h5>', unsafe_allow_html=True)
-    with clms12[1]: 
-        st.video('https://youtu.be/rp3AMCdKxdI?si=mVT9zedbk_dRI2Gj')
-    clms13 = st.columns([1,1])
-    with clms13[0]:
-        st.title('')
-        st.markdown('<h5>Aun Más Frases</h5>', unsafe_allow_html=True)
-    with clms13[1]: 
-        st.video('https://youtu.be/ldSbwWtKf6A?si=QSiW_7Rc-mJXZCHM')
+    st.text("Extras")
     clms14 = st.columns([1,1])
     with clms14[0]:
         st.title('')
         st.markdown('<h5>Libro</h5>', unsafe_allow_html=True)
     with clms14[1]: 
-        st.video('https://youtu.be/BlPrDMZUzkY')    
+        st.video('https://www.youtube.com/watch?v=qp9jl0ECUwA')
     clms15 = st.columns([1,1])
     with clms15[0]:
         st.title('')
-        st.markdown('<h5>Otra Frases</h5>', unsafe_allow_html=True)
+        st.markdown('<h5>Frases Extra</h5>', unsafe_allow_html=True)
     with clms15[1]: 
-        st.video('https://youtu.be/CcxmBTfDlMo')   
+        st.video('https://www.youtube.com/watch?v=CcxmBTfDlMo')
+
     st.divider()
 
     st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
-    clms15 = st.columns([1,1])
-    with clms15[0]:
+    clms18 = st.columns([1,1])
+    with clms18[0]:
         st.title('')
-        st.markdown('<h5>Nuevo Vocabulario</h5>', unsafe_allow_html=True)
-    with clms15[1]:
-        st.video('https://youtu.be/9q4e_fQB6No')
+        st.markdown('<h5>Practicar Vocabulario</h5>', unsafe_allow_html=True)
+    with clms18[1]:
+        st.video('https://www.youtube.com/watch?v=RNjed7e9vXM')
+    # clms19 = st.columns([1,1])
+    # with clms19[0]:
+    #     st.title('')
+    #     st.markdown('<h5>Vocabulario para la Semana que Viene</h5>', unsafe_allow_html=True)
+    # with clms19[1]:
+    #     st.video('https://www.youtube.com/watch?v=RNjed7e9vXM')
     st.divider()
     
-    components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQDz2mgnEZK_yCCscpVGqeYxXX1ZYHPER5m1VkECKBv_IPBkdw-RHUf-0kLOI2gBkwx94Cj0reBjJ4W/embed?start=false&loop=false&delayms=3000", height=480)
+    components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vT7ivxIqAqCRPzOxAn5dHtu8vnp2-qZ7qcX2GKvBF38GLR1GkytUeJAw6-OEZUWkfR3xKLeDGQpc1Hm/embed?start=false&loop=false&delayms=3000", height=480)
+    # clms20 = st.columns([1,1])
+    # with clms20[0]:
+    #     st.title('')
+    #     st.markdown('<h5>Grabacion de la Clase</h5>', unsafe_allow_html=True)
+    # with clms20[1]: 
+    #     st.video('https://www.youtube.com/watch?v=BtEIgRvVGg8')
+    st.divider()
+
+    
 
 main()
