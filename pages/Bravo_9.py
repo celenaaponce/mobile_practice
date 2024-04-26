eb248c4423168c0d2b220a8495740c4ab4404085
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-04-26 11:49:39 -0700

    Update Bravo_9.py

--- pages/Bravo_9.py
+++ pages/Bravo_9.py
@@ -64,7 +64,7 @@ def banco1():
         st.title('')
         st.markdown('<h5>Carreras para Personaas Sordos</h5>', unsafe_allow_html=True)
     with clms29[1]:
-        st.video('https://www.youtube.com/watch?v=yrrwxrb8JRQ')
+        st.video('https://youtu.be/BTbqe7AgkyI')
 
     st.divider()
 
@@ -81,8 +81,8 @@ def banco1():
         st.title('')
         st.markdown('<h5>Practica</h5>', unsafe_allow_html=True)
     with clms28[1]:
-        st.markdown("<a href='https://edpuzzle.com/media/661d4fbc6190b34cdf780418' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/sandbox/main/images/puzzle.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
-        st.markdown("<a href='https://edpuzzle.com/media/661d5a8f607cee61aa154c91' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/sandbox/main/images/puzzle.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
+        st.markdown("<a href='https://edpuzzle.com/media/661d4fbc6190b34cdf780418' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/puzzle.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
+        st.markdown("<a href='https://edpuzzle.com/media/661d5a8f607cee61aa154c91' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/puzzle.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
 
     st.divider()
             
@@ -96,78 +96,78 @@ def banco1():
             
 def banco2():
     style_html = set_styles('#94387f')
-    st.write(style_html, unsafe_allow_html=True)
-    st.subheader('Lección 9: Banco')
-    st.markdown("<h4 style='text-align: center; color: white;'><u>Videos</u></h4>", unsafe_allow_html=True)
-    clms21 = st.columns([1,1])
-    with clms21[0]:
-        st.title('')
-        st.markdown('<h5>Vocabulario </h5>', unsafe_allow_html=True)
-    with clms21[1]:
-        st.video('https://youtu.be/NPxK-atgF_s')
-    clms22 = st.columns([1,1])
-    with clms22[0]:
-        st.title('')
-        st.markdown('<h5>Más Vocabulario </h5>', unsafe_allow_html=True)
-    with clms22[1]:
-        st.video('https://youtu.be/NyzYX41Tbcs')
-    clms23 = st.columns([1,1])
-    with clms23[0]:
-        st.title('')
-        st.markdown('<h5>Repaso y Explicacion</h5>', unsafe_allow_html=True)
-    with clms23[1]:
-        st.video('https://youtu.be/Tros4qGos-s')
-    clms29 = st.columns([1,1])
-    with clms29[0]:
-        st.title('')
-        st.markdown('<h5>Más Repaso y Explicacion</h5>', unsafe_allow_html=True)
-    with clms29[1]:
-        st.video('https://youtu.be/byFIxaEHutY')
-    clms26 = st.columns([1,1])
-    with clms26[0]:
-        st.title('')
-        st.markdown('<h5>Gramatica</h5>', unsafe_allow_html=True)
-    with clms26[1]:
-        st.video('https://youtu.be/BDp8OlEOOFE')
-    clms29 = st.columns([1,1])
-    with clms29[0]:
-        st.title('')
-        st.markdown('<h5>Frases</h5>', unsafe_allow_html=True)
-    with clms29[1]:
-        st.video('https://youtu.be/2Ljs6KdnO3s')
-    clms30 = st.columns([1,1])
-    with clms30[0]:
-        st.title('')
-        st.markdown('<h5>Cuento (sin subtitulos) 🔇</h5>', unsafe_allow_html=True)
-    with clms30[1]:    
-        st.video('https://www.youtube.com/watch?v=u0hXZ-vBpJ8')
-    clms31 = st.columns([1,1])
-    with clms31[0]:
-        st.title('')
-        st.markdown('<h5>Cuento (con subtitulos) 🔈</h5>', unsafe_allow_html=True)
-    with clms31[1]: 
-        st.video('https://youtu.be/Dce7myq8rao')
-    clms32 = st.columns([1,1])
-    with clms32[0]:
-        st.title('')
-        st.markdown('<h5>Carreras para Personas Sordos</h5>', unsafe_allow_html=True)
-    with clms32[1]: 
-        st.video('https://www.youtube.com/watch?v=yrrwxrb8JRQ')      
-    st.divider()
+    # st.write(style_html, unsafe_allow_html=True)
+    # st.subheader('Lección 9: Banco')
+    # st.markdown("<h4 style='text-align: center; color: white;'><u>Videos</u></h4>", unsafe_allow_html=True)
+    # clms21 = st.columns([1,1])
+    # with clms21[0]:
+    #     st.title('')
+    #     st.markdown('<h5>Vocabulario </h5>', unsafe_allow_html=True)
+    # with clms21[1]:
+    #     st.video('https://youtu.be/NPxK-atgF_s')
+    # clms22 = st.columns([1,1])
+    # with clms22[0]:
+    #     st.title('')
+    #     st.markdown('<h5>Más Vocabulario </h5>', unsafe_allow_html=True)
+    # with clms22[1]:
+    #     st.video('https://youtu.be/NyzYX41Tbcs')
+    # clms23 = st.columns([1,1])
+    # with clms23[0]:
+    #     st.title('')
+    #     st.markdown('<h5>Repaso y Explicacion</h5>', unsafe_allow_html=True)
+    # with clms23[1]:
+    #     st.video('https://youtu.be/Tros4qGos-s')
+    # clms29 = st.columns([1,1])
+    # with clms29[0]:
+    #     st.title('')
+    #     st.markdown('<h5>Más Repaso y Explicacion</h5>', unsafe_allow_html=True)
+    # with clms29[1]:
+    #     st.video('https://youtu.be/byFIxaEHutY')
+    # clms26 = st.columns([1,1])
+    # with clms26[0]:
+    #     st.title('')
+    #     st.markdown('<h5>Gramatica</h5>', unsafe_allow_html=True)
+    # with clms26[1]:
+    #     st.video('https://youtu.be/BDp8OlEOOFE')
+    # clms29 = st.columns([1,1])
+    # with clms29[0]:
+    #     st.title('')
+    #     st.markdown('<h5>Frases</h5>', unsafe_allow_html=True)
+    # with clms29[1]:
+    #     st.video('https://youtu.be/2Ljs6KdnO3s')
+    # clms30 = st.columns([1,1])
+    # with clms30[0]:
+    #     st.title('')
+    #     st.markdown('<h5>Cuento (sin subtitulos) 🔇</h5>', unsafe_allow_html=True)
+    # with clms30[1]:    
+    #     st.video('https://www.youtube.com/watch?v=u0hXZ-vBpJ8')
+    # clms31 = st.columns([1,1])
+    # with clms31[0]:
+    #     st.title('')
+    #     st.markdown('<h5>Cuento (con subtitulos) 🔈</h5>', unsafe_allow_html=True)
+    # with clms31[1]: 
+    #     st.video('https://youtu.be/Dce7myq8rao')
+    # clms32 = st.columns([1,1])
+    # with clms32[0]:
+    #     st.title('')
+    #     st.markdown('<h5>Carreras para Personas Sordos</h5>', unsafe_allow_html=True)
+    # with clms32[1]: 
+    #     st.video('https://www.youtube.com/watch?v=yrrwxrb8JRQ')      
+    # st.divider()
 
-    st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
+    # st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
 
-    clms28 = st.columns([1,1])
-    with clms28[0]:
-        st.title('')
-        st.markdown('<h5>Practica</h5>', unsafe_allow_html=True)
-    with clms28[1]:
-        st.markdown("<a href='https://edpuzzle.com/media/661ebee40c33da0da686b776' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/puzzle.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
-        st.markdown("<a href='https://edpuzzle.com/media/661ea7be7d0bcf016e521bd2' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/puzzle.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
+    # clms28 = st.columns([1,1])
+    # with clms28[0]:
+    #     st.title('')
+    #     st.markdown('<h5>Practica</h5>', unsafe_allow_html=True)
+    # with clms28[1]:
+    #     st.markdown("<a href='https://edpuzzle.com/media/661ebee40c33da0da686b776' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/puzzle.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
+    #     st.markdown("<a href='https://edpuzzle.com/media/661ea7be7d0bcf016e521bd2' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/puzzle.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
 
-    st.divider()
+    # st.divider()
             
-    components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vRTGMfYP-QC31amUPCsEMYFODxvfZ6uVLBwjpo6sAKgTtw07e23EEF0DMMSmEtBYGGP_xX6X8VGWckv/embed?start=false&loop=false&delayms=3000", height=480)
+    # components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vRTGMfYP-QC31amUPCsEMYFODxvfZ6uVLBwjpo6sAKgTtw07e23EEF0DMMSmEtBYGGP_xX6X8VGWckv/embed?start=false&loop=false&delayms=3000", height=480)
     # clms29 = st.columns([1,1])
     # with clms29[0]:
     #     st.title('')
