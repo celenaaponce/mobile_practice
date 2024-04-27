ff49e10ae17df620ab5a05002513737b81f39833
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-04-26 19:04:35 -0700

    Update nav.py\

--- modules/nav.py
+++ modules/nav.py
@@ -31,7 +31,7 @@ def ASL1_sidebar():
             st.sidebar.page_link("pages/Introduccion_a_ASL_1.py", label="Introducción a ASL 1")
             st.sidebar.page_link("pages/Bravo_1.py", label="Conocer la Familia Bravo")
             st.sidebar.page_link("pages/Bravo_2.py", label="Desayuno")
-            st.sidebar.page_link("pages/Bravo_3.py", label="Dónde está el control?")
+            st.sidebar.page_link("pages/Bravo_3.py", label="¿Dónde está el contról?")
             st.sidebar.page_link("pages/holidays_spring.py", label="Días Festivos")
             
 def ASL2_sidebar():
