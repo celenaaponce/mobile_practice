e00ecc3203c645509c46b52673133cba1e16d6e7
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-05-10 14:49:37 -0700

    Update Recursos.py

--- pages/Recursos.py
+++ pages/Recursos.py
@@ -67,3 +67,9 @@ st.markdown("<h3>Council de Manos</h3>\
               <p> <a href='https://www.facebook.com/councildemanos' target='_blank'>Council de Manos</a> es una organización sin \
                 ánimo de lucro para la comunidad latinx sorda, originalmente se llamaba Consejo Nacional de Sordos y Hipoacúsicos \
                     Hispano; “NCDHH”. </p>", unsafe_allow_html=True)
+
+st.divider()
+st.markdown("<h3>Recursos para Negocios Sin Ánimo de Lucro</h3>\
+              <p> <a href='https://cms.childtrends.org/wp-content/uploads/2016/09/2016-51LatinoCommunicationsGuide.pdf' target='_blank'>El Instituto Hispano <\a>ha elaborado una guía para trabajar \
+              con las comunidades hispanohablantes. En ella se explican las prácticas adecuadas y se ofrece una \
+              infraestructura de comunicación. . </p>", unsafe_allow_html=True)
