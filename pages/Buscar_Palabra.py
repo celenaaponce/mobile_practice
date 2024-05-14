d4a72699bc3d58a2e9bd724b3098f92cd58de6bf
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-05-14 16:17:11 -0700

    Update Buscar_Palabra.py

--- pages/Buscar_Palabra.py
+++ pages/Buscar_Palabra.py
@@ -6,7 +6,8 @@ import streamlit as st
 from streamlit import session_state as ss
 from modules.nav import MenuButtons
 from pages.account import get_roles
-
+from check.spanish_word_freq import SpanishWordFreq
+from check.word_chekcer import WordChecker
 
 st.set_page_config(layout="wide", page_title="Buscar Palabra")
 MenuButtons('')
@@ -47,6 +48,15 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+def replace_dimensions(template):
+    x = "width='410'"
+    y = "height='317'"    
+    return template.replace("width='x'", str(x)).replace("height='y'", str(y))
+
+def replace_dimensions_img(template):
+    z = "width=300"  
+    return template.replace("width=z", str(z)) 
+    
 @st.cache_data
 def download_csv(file_id, output_file):
     path = f'https://drive.google.com/uc?export=download&id={file_id}'
@@ -76,11 +86,12 @@ if 'download' not in st.session_state:
     
 #start with download
 if st.session_state.download == False:
-  download_csv(st.secrets['diccionario_letras'], 'Search List2.csv')
+  download_csv(st.secrets['diccionario_test'], 'Search List2.csv')
 
-word_data = download_csv(st.secrets['diccionario_letras'], 'Search List2.csv')
+word_data = download_csv(st.secrets['diccionario_test'], 'Search List2.csv')
 word_data = word_data[['Palabra', 'Imagen', 'Video', 'Tema', 'Sinómino']]
-
+word_data['Video'] = word_data['Video'].apply(replace_dimensions)
+word_data['Imagen'] = word_data['Imagen'].apply(replace_dimensions_img)
 st.write("")
 st.header("Buscar Palabra")
 word = st.text_input("Buscar Palabra", label_visibility="hidden")
@@ -103,6 +114,20 @@ if not word_list.empty:
 else:
     word_data['Palabra'] = word_data['Palabra'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
     word_list = word_data.loc[word_data['Palabra']==word]
+    if not word_list.empty:
+        table = word_list.to_html(classes='mystyle', escape=False, index=False)
+    
+        html_string = f'''
+    
+            <body>
+                {table}
+            </body>
+            '''
+        st.markdown(
+                html_string,
+            unsafe_allow_html=True)
+if word_list.empty and word != "":
+    word_list = word_data.loc[word_data['Palabra'].str.contains(word)]
     table = word_list.to_html(classes='mystyle', escape=False, index=False)
 
     html_string = f'''
@@ -113,10 +138,9 @@ else:
         '''
     st.markdown(
             html_string,
-        unsafe_allow_html=True)
-    
+        unsafe_allow_html=True)     
 if word_list.empty and word != "":
-    filePath = "pages/10000_frecuencias.txt"
+    filePath = "pages/helper/10000_frecuencias.txt"
 
     spanishWords = SpanishWordFreq(filePath)
     
