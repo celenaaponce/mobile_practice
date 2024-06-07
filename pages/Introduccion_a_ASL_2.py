20f574a7d12b348343c964aeb014c3d725f72207
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-06-06 17:27:09 -0700

    Update Introduccion_a_ASL_2.py

--- pages/Introduccion_a_ASL_2.py
+++ pages/Introduccion_a_ASL_2.py
@@ -1,6 +1,4 @@
 import streamlit as st
-from PIL import Image
-from streamlit_extras.switch_page_button import switch_page
 import streamlit.components.v1 as components
 from pages.sidebars import login_sidebar_ASL2, set_styles
 from modules.nav import MenuButtons
