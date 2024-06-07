107ac898a189a9586c7c16258b53a2ffbbc38f02
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-06-06 20:31:49 -0700

    Update holidays_spring_3.py

--- pages/holidays_spring_3.py
+++ pages/holidays_spring_3.py
@@ -1,8 +1,5 @@
 import streamlit as st
-from PIL import Image
-from streamlit_extras.switch_page_button import switch_page
 import streamlit.components.v1 as components
-from pages.sidebars import login_sidebar_ASL3, set_styles
 from modules.nav import MenuButtons
 st.markdown("""
     <style>
